fd01ccd16d43a970941c82b000c58de0e9f95b21
zkim <zandergunn@gmail.com>
2024-04-29 13:15:12 -0700

    MERN-Stack Infrastructure

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+// App.jsx
+// Add the following import
+import { getUser } from '../../utilities/users-service';
+
+
+export default function App() {
+  const [user, setUser] = useState(getUser());
+  return (
+    <main className="App">
+      App
+    </main>
+  );
+}
